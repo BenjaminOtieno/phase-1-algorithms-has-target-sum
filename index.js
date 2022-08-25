36b6d40dcad1a84e33307ac015ff0f684601acc8
BenjaminOtieno <benjamin.otieno@moringaschool.com>
2022-08-25 15:13:12 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // iterate over the array of no.
+  for (let i = 0; i < array.length; i++) {
+    // for the current number, identify a complementary no.that adds to our target
+    // (eg: if our no. is 2, and the target is 5, the complementary no. is 3)
+    const complement = target - array[i];
+    // iterate over the remaining no.s in the array
+    for (let j = i + 1; j < array.length; j++) {
+      // check if any of the remaining no.s is the complement
+      // if so, return true
+      if (array[j] === complement) return true;
+    }
+  }
+  // if we reach the end of the array, return false
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
@@ -32,3 +33,25 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
+
+//OR
+
+
+function hasTargetSum(array, target) {
+  // create an object to keep track of all the no.s we've seen
+  const seenNumbers = {};
+  // iterate over the array of no.s
+  for (const number of array) {
+    // for the current no, identify a complementary no that adds to our target
+    // (for example: if our no is 2, and the target is 5, the complementary no is 3)
+    const complement = target - number;
+    // check if any of the keys in our object is the complement to the current no
+    // if so, return true
+    if (seenNumbers[complement]) return true;
+    // save the current no as the key on our object so we can check it later against other no.s
+    seenNumbers[number] = true;
+  }
+  // if we reach the end of the array, return false
+  return false;
+}
